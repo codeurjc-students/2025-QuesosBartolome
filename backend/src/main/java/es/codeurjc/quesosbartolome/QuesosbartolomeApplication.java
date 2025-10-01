05f52a1bd7ecac33020190fd089b965637332a5c
victorino2324 <victorbl2004@gmail.com>
2025-10-01 18:24:31 +0200

    add backend and frontend

---
+++ backend/src/main/java/es/codeurjc/quesosbartolome/QuesosbartolomeApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.quesosbartolome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuesosbartolomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuesosbartolomeApplication.class, args);
+	}
+
+}
