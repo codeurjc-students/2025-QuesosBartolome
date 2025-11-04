aa65d0d242f0cd9b40a1c329ad9380e3f353bb0b
victorino2324 <victorbl2004@gmail.com>
2025-11-04 16:21:02 +0100

    add styles

--- frontend/src/app/components/cheese-list.component.ts
+++ frontend/src/app/components/cheese-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbDropdownModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { CheeseService } from '../service/cheese.service';
 import { CheeseDTO } from '../dto/cheese.dto';
 
 @Component({
   selector: 'app-cheese-list',
-  imports: [CommonModule],
-  templateUrl: './cheese-list.component.html'
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgbDropdownModule,
+    NgbTooltipModule
+  ],
+  templateUrl: './cheese-list.component.html',
+  styleUrls: ['./cheese-list.component.css']
 })
 export class CheeseListComponent implements OnInit {
-
   cheeses: CheeseDTO[] = [];
 
   constructor(private cheeseService: CheeseService) {}
@@ -22,5 +28,9 @@ export class CheeseListComponent implements OnInit {
       },
       error: (err) => console.error('Error loading cheeses', err)
     });
+  }
+
+  openNewCheeseModal(): void {
+    alert('Abrir modal para nuevo queso');
   }
 }
