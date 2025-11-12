7807e12959ce80ff7fef1b08cc3d03f1e0ee38c0
victorino2324 <victorbl2004@gmail.com>
2025-11-12 16:34:06 +0100

    fix2

--- frontend/karma.conf.ts
+++ frontend/karma.conf.ts
@@ -1,27 +1,36 @@
 // @ts-nocheck
-import type { Config } from 'karma';
+import type { Config } from "karma";
+import * as path from "path";
 
-export default function (config: Config) {
+export default function(config: Config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
 
-    reporters: ['progress', 'kjhtml'],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma")
+    ],
 
-    browsers: ['ChromeHeadlessNoSandbox'],
+    reporters: ["progress", "kjhtml"],
+
+    browsers: ["ChromeHeadlessNoSandbox"],
 
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
+        base: "ChromeHeadless",
         flags: [
-          '--headless=new',
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--ignore-certificate-errors',
-          '--allow-insecure-localhost',
-          '--disable-web-security'
-        ],
-      },
+          "--headless=new",
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--ignore-certificate-errors",
+          "--allow-insecure-localhost",
+          "--disable-web-security"
+        ]
+      }
     },
 
     client: {
@@ -29,7 +38,17 @@ export default function (config: Config) {
       clearContext: false
     },
 
-    singleRun: true,
+    coverageReporter: {
+      dir: path.join(__dirname, "./coverage"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }]
+    },
+
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
     autoWatch: false,
+    singleRun: true,
+    restartOnFileChange: false
   });
 }
