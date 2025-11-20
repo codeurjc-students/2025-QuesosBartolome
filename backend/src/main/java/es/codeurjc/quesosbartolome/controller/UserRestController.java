c5bb7f34cf0bfd7c66848fc5e544a0fb38b7d091
victorino2324 <victorbl2004@gmail.com>
2025-11-21 00:45:04 +0100

    .

--- backend/src/main/java/es/codeurjc/quesosbartolome/controller/UserRestController.java
+++ backend/src/main/java/es/codeurjc/quesosbartolome/controller/UserRestController.java
@@ -35,6 +35,7 @@ public class UserRestController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You must be authenticated");
+        
     }
 
     
